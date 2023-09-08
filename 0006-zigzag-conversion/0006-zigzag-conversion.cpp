2f51acb3acb3b7e76469473ab956f89d7c8c0646
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2023-09-07 17:13:19 -0700

    Time: 11 ms (56.14%), Space: 10.3 MB (68.66%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,33 +1,33 @@
-#include <cstdio>
+
 
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        
-        //Initialize values
-        vector<string> rows (numRows, "");
-        int row = 0;
-        int direction = 1;
-        string solution = "";
 
-        for(int i = 0; i < s.length(); i++){
-            if(numRows < 2){direction = 0;}
-            else if(row < 0 || row > numRows - 1){
-                direction *= -1;
-                row += direction;
-                row += direction;
-            }
-            
-            rows[row].push_back(s[i]);
-            //cout << row << " : " << rows[row] << endl;
-            row += direction;
-            
-            
-        }
-        
-        for(int j = 0; j < numRows; j++){
-            solution += rows[j];
-        }
-        return solution;
+    string convert(string s, int numRows) {
+    
+    if(numRows <= 1) return s;
+
+    vector<string>v(numRows, ""); 
+
+    int j = 0, dir = -1;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+        if(j == numRows - 1 || j == 0) dir *= (-1); 
+		 
+        v[j] += s[i];
+
+        if(dir == 1) j++;
+
+        else j--;
+    }
+
+    string res;
+
+    for(auto &it : v) res += it; 
+
+    return res;
+
     }
 };

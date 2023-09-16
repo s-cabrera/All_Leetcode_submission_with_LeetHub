32329d153bb8ece381e12270d602deac8a8de3c3
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2023-09-15 19:23:09 -0700

    Time: 0 ms (100%), Space: 6.2 MB (6.26%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,12 +3,12 @@ public:
     int reverse(int x) {
         int ans = 0;
         int sign = (x > 0) ? 1: -1;
-        x = abs(x);
+        int input = abs(x);
 
-        while (x){
+        while (input){
             if((ans * sign) < INT_MIN /10  || (ans * sign) > INT_MAX / 10) return 0;
-            ans = (ans * 10) + (x % 10);
-            x = x /10;
+            ans = (ans * 10) + (input % 10);
+            input = input /10;
         }
         return ans * sign;
     }
